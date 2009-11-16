edc901c48f931fc9a39ab8c2b122ef0e2e7874a2
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2009-11-16 15:31:19 +0100

    A bit of doc'ing.

--- OIApplication.h
+++ OIApplication.h
@@ -9,38 +9,89 @@
 #import <Foundation/Foundation.h>
 #import "OIResponder.h"
 
+/**
+ \file
+ 
+ Functions and classes in this file initialize and run the application and its main loop.
+ */
+
+
+/**
+ Initializes the Objective-C runtime and Ecore. This function must be called before making any Objective-C call in main(), and it does not need an autorelease pool to be in place.
+ */
 extern void OIInitializeProcess(int argc, const char* argv[]);
 
+/**
+ A shortcut for [OIApplication sharedApplication].
+ */
 #define OIApp ((OIApplication*)[OIApplication sharedApplication])
 
 // -----
 
-@protocol OIApplicationDelegate;
-
+/**
+ This notification is sent just before the application terminates. See OIApplicationDelegate::applicationWillTerminate: for more information.
+ */
 extern NSString* const OIApplicationWillTerminateNotification;
 
+/**
+ An application object represents your app. Application objects run the run loop and allow for graceful app shutdown when needed.
+
+ Application objects, just like in AppKit or UIKit, aren't usually subclassed. Instead, they send messages to another object, the application delegate. OIApplication retains its delegate.
+ 
+ The application object is a responder and is intended to be the "top" of all responder chains (that is, the object all events are ultimately forwarded to if unhandled). By default, it does not declare the use of any OIMap and has no intent delegate.
+ 
+ Applications are singletons; there is only one application instance in each app. The singleton is created the first time the OIApplication::sharedApplication method is called, and is of the class it is first called on (OIApplication or the desired subclass, if any).
+ */
 @interface OIApplication : OIResponder {
 	id delegate;
 	BOOL shouldTerminate;
-	
-	NSTimer* loopTasker;
 }
 
+/**
+ Returns or creates, if missing, the application singleton. You can use the OIApp macro as a shortcut to invoke this method.
+ */
 + sharedApplication;
 
+/**
+ The application delegate. You should not change this value after the application starts running (that is, after OIApplication::run is called).
+ */
 @property(retain) id delegate;
+
+/**
+ Runs the application. This makes the run loop spin and starts sending appropriate messages to the application delegate.
+ This method does not return. See OIApplicationDelegate::applicationWillTerminate for more information on how the app is termianted.
+ */
+- (void) run;
+
+/**
+ Convenience method that creates a new object of the given class, sets it as the delegate, then runs the application with -run.
+ */
 - (void) runWithDelegateClass:(Class) c;
 
-- (void) run;
+/**
+ Terminates the application at the earliest possible time, usually after the current event has finished processing. This method may terminate the application immediately if it's safe to do so, but may pospone this in order to correctly end the underlying Enlightenment run loop.
+ 
+ That is: do not assume this method will or will not return after used. It's often wise to return from the caller immediately after calling it.
+ */
 - (void) terminateWhenPossible;
 
 @end
 
 // -----
 
+/**
+ These are the methods that the delegate of a OIApplication can receive.
+ */
 @interface NSObject (OIApplicationDelegate)
 
+/**
+ Sent before the first event is processed. This is usually where the UI and application controllers are first set up.
+ */
 - (void) applicationDidFinishLaunching:(OIApplication*) app;
+
+/**
+ Sent just before the application is terminated. The OIApplication terminates the app by sending this message (and the associated OIApplicationWillTerminateNotification) and then calling the exit() function from within the implementation of the OIApplication::run method; this means that the dealloc method will not be called on objects still "alive" after this method returns. This matches the behavior of AppKit and UIKit and makes shutdown much faster.
+ */
 - (void) applicationWillTerminate:(OIApplication*) app;
 
 @end
--- Ohai.h
+++ Ohai.h
@@ -1,3 +1,14 @@
+/**
+ \mainpage
+ 
+ Ohai is a library designed to be used in OpenInkpot.org devices to create applications for e-book readers. It resembles Cocoa's AppKit and Cocoa Touch's UIKit in general structure and use, and borrows many terms and design decisions from these two frameworks while mapping them over OpenInkpot's preferred Enlightenment libraries (especially Ecore and Evas).
+ 
+ An application is represented in Ohai by an instance of the OIApplication class; this application manages one or more windows, whose contents are drawn by using view objects. A window corresponds to an Ecore_Evas instance, while a view (with many caveats) to a Evas_Object instance.
+ 
+ Events are relayed through a responder chain similar to Cocoa's, but are not encapsulated in object; rather, an event is represented by a message sent to a responder object, which can relay this message to its next responder if desired (typically if the event was not handled). Events are "raw" -- stimuli coming from the user without interpretation -- but OIResponder has a facility that can allow the responder to map the event to an "intent" depending on the device the app runs on. A responder uses one or more OIMap objects representing the logical intents the user may express and can optionally relay those messages to an "intent delegate", allowing a controller to receive a view's events.
+ 
+ */
+
 #import <Ohai/OIApplication.h>
 #import <Ohai/OIScreen.h>
 #import <Ohai/OIWindow.h>
